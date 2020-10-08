19952154c6fffe946c3bbffacbe63a90d68cddb4
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-10-07 17:12:53 -0700

    added dependency packages and set up routes

---
+++ portfolio_react/src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Switch,
+    Route
+} from 'react-router-dom'
+
+
+function BaseRouter() {
+    return(
+        <div>
+            <Switch>
+
+            </Switch>
+        </div>
+    )
+}
+
+export default BaseRouter;

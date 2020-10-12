7c0f8e9ec89729f1003f769afbcd467bbbd661c4
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-10-11 22:50:59 -0700

    removed default home page, imported BaseRouter

--- portfolio_react/src/App.js
+++ portfolio_react/src/App.js
@@ -1,31 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
 import {BrowserRouter as Router} from "react-router-dom"
 import 'antd/dist/antd.css';
 import CustomLayout from './components/layout/CustomLayout'
+import BaseRouter from './routes'
 
 
 function App() {
   return (
     <Router>
       <CustomLayout>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <BaseRouter />
       </CustomLayout>
     </Router>
   );
